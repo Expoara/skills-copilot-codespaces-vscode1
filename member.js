58a5814f42f392d6da096d52c3185ac2c832ed07
Ramboelagil <expoara@gmail.com>
2024-06-17 14:38:20 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,6 @@
+function skillsMember() {
+    let skills = ['JavaScript', 'React', 'React Native', 'NodeJS', 'MongoDB'];
+    let age = 25;
+    let name = 'John Doe';
+    return { skills, age, name };
+}
